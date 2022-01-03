e814aff22a652c3ab7dcaf757a8b7b68e8465be4
Oliver Day <oliver.day@usc.edu>
2022-01-02 18:02:10 -0800

    Add npm packages and starter version of 'App' component

---
+++ rejection/pages/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from '../styles/Home.module.css'
+
+function App() {
+return (
+    <div className={styles.container}>
+        <div className="app">
+            Hello there Oliver!
+        </div>      
+    </div>
+);
+}
+  
+export default App;
