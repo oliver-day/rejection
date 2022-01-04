f3eb15255eded91f12c5520d718e2144eee3601b
Oliver Day <oliver.day@usc.edu>
2022-01-03 17:27:35 -0800

    Remove 'devToolsEnhancer'

--- rejection/redux/store.js
+++ rejection/redux/store.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { createWrapper } from "next-redux-wrapper"
-import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import rootReducer from './reducers';
 
 
-const middleware = [devToolsEnhancer];
+const middleware = [];
 
 const makeStore = () => createStore(rootReducer, compose(applyMiddleware(...middleware)));
 
