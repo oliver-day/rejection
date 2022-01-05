83418035570fa996cb44f2f3dd1cf2f14fc21ba0
Oliver Day <oliver.day@usc.edu>
2022-01-04 16:39:09 -0800

    Add saga test for syncing state to localStorage

--- rejection/src/features/rejection/saga.test.js
+++ rejection/src/features/rejection/saga.test.js
@@ -1,44 +1,48 @@
 import { describe } from 'riteway';
-import { call, put, take } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 
 import {
     rejectionReducer,
     rejectionSlice,
     hydrateQuestionsSucceeded,
-    createQuestion
+    createQuestion,
+    getQuestions
 } from './reducer';
-import { hydrateLocalState } from './saga';
-import { loadState } from '../../../store/localStorage';
+import {
+    hydrateLocalState,
+    syncLocalState
+} from './saga';
+import { loadState, saveState } from '../../../store/localStorage';
 
 
 const withSlice = state => ({ [rejectionSlice]: state });
 
-describe('rejection/saga | hydrateLocalState (w/no state in localStorage)', async assert => {
+describe('rejection/saga | hydrateLocalState (wo/ state)', async assert => {
     const gen = hydrateLocalState();
 
     assert({
-        given: 'first yield statement of`hydrateLocalState` saga` (wo/ state in localStorage)',
+        given: 'first yield statement of`hydrateLocalState` saga` (wo/ state)',
         should: 'the `loadState` function should be called first',
         actual: gen.next().value,
         expected: call(loadState)
     });
 
     assert({
-        given: 'second yield statement `hydrateLocalState` saga`',
-        should: 'put the`hydrateQuestionsSucceeded` function with undefined state (due to no state in localStorage)',
+        given: 'second yield statement `hydrateLocalState` saga` (wo/ state)',
+        should: 'put the`hydrateQuestionsSucceeded` function with undefined state',
         actual: gen.next(undefined).value,
         expected: put(hydrateQuestionsSucceeded())
     });
 
     assert({
-        given: 'no remaining yield statements  `hydrateLocalState` saga`',
-        should: '`hydrateQuestionsSucceeded` function should be done (wo/ state in localStorage)',
+        given: 'no remaining yield statements  `hydrateLocalState` saga` (wo/ state',
+        should: 'now be done',
         actual: gen.next(),
         expected: { done: true, value: undefined }
     });
 });
 
-describe('rejection/saga | hydrateLocalState (w/ state in localStorage)', async assert => {
+describe('rejection/saga | hydrateLocalState (w/ state)', async assert => {
     
     const question = {
         question: "May I have time off?",
@@ -50,22 +54,56 @@ describe('rejection/saga | hydrateLocalState (w/ state in localStorage)', async 
     const gen = hydrateLocalState();
 
     assert({
-        given: 'first yield statement of`hydrateLocalState` saga` (w/ state in localStorage)',
+        given: 'first yield statement of`hydrateLocalState` saga` (w/ state)',
         should: 'the `loadState` function should be called first',
         actual: gen.next().value,
         expected: call(loadState)
     });
 
     assert({
-        given: 'second yield statement `hydrateLocalState` saga`',
+        given: 'second yield statement `hydrateLocalState` saga` (w/ state)',
         should: 'put the`hydrateQuestionsSucceeded` function with loaded state',
         actual: gen.next(state).value,
         expected: put(hydrateQuestionsSucceeded(state))
     });
     
     assert({
-        given: 'no remaining yield statements  `hydrateLocalState` saga`',
-        should: '`hydrateQuestionsSucceeded` function should be done (w/ state in loaded)',
+        given: 'no remaining yield statements  `hydrateLocalState` saga` (w/ state)',
+        should: 'should now be done (w/ state loaded)',
+        actual: gen.next(),
+        expected: { done: true, value: undefined }
+    });
+});
+
+
+describe('rejection/saga | syncLocalState (save state to localStorage)', async assert => {
+    
+    const question = {
+        question: "May I have time off?",
+        askee: "Boss",
+        status: "Unanswered",
+    };
+    const state = withSlice(rejectionReducer(rejectionReducer(), createQuestion(question)));
+    
+    const gen = syncLocalState();
+
+    assert({
+        given: 'first yield statement of`syncLocalState` saga',
+        should: 'select state w/ `getQuestions` function',
+        actual: gen.next().value,
+        expected: select(getQuestions)
+    });
+
+    assert({
+        given: 'second yield statement `syncLocalState` saga`',
+        should: 'call the`saveState` function with the selected state',
+        actual: gen.next(state).value,
+        expected: call(saveState, state)
+    });
+
+    assert({
+        given: 'no remaining yield statements  `syncLocalState` saga`',
+        should: 'should now be done',
         actual: gen.next(),
         expected: { done: true, value: undefined }
     });
