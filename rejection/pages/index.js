6c97619f535feb0d25a3e7bc2074c6147d14c619
Oliver Day <oliver.day@usc.edu>
2022-01-11 13:44:10 -0800

    Remove unnecessary imports

--- rejection/pages/index.js
+++ rejection/pages/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import App from '../src/App';
-import { rejectionReducer, rejectionSlice } from '../src/features/rejection/reducer';
 
 export default function Index() {
   return (
